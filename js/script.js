2d521dbffe3efbae5d57c0d010a6ab47c3e641a2
Glitch (tangible-political-tumbleweed) <none>
2021-04-22 23:34:34 +0000

    ☕️💏 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let gifLink;
 $(".search-button").click(function(){
   userSearch = $(".search-term").val();
   userapiURL = "https://api.giphy.com/v1/gifs/search?q=" + userSearch + "&rating=pg&api_key=dc6zaTOxFJmzC";
-  console.log(userapiURL);
+  // console.log(userapiURL);
   
   
   fetch(`${userapiURL}`)
@@ -16,8 +16,8 @@ $(".search-button").click(function(){
        })
       .then(function(data) {
           randomNumber = Math.floor(Math.random() * 50);
-          gifLink = data.data[randomNumber].url;
-          console.log(gifLink);
+          gifLink = data.data[randomNumber].images.original.url;
+          // console.log(gifLink);
           $(".main").html(`<img src=${gifLink}>`);
       });
   
